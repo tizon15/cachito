e9be7a9eb6d93a64b085f05e1d6b7f4799c0eb10
Álvaro Martínez-Tizón García <amartineztizon@gmail.com>
2023-05-11 10:01:01 +0200

    initial commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cachito-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.less']
+})
+export class NavbarComponent {
+
+}
